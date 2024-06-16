5866f92c87114f8a6d3de2cf2423084bcefaa38b
Kasia <kasia1781@gmail.com>
2024-06-16 19:24:11 +0200

    using useNavigation hook

--- frontend/src/pages/RooyLayout.tsx
+++ frontend/src/pages/RooyLayout.tsx
@@ -1,11 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
 
+//aby sprawdzić aktualny stan ładowania danych uzywamy hook useNavigation.
+
 export default function RootLayout() {
+	const navigation = useNavigation();
+
 	return (
 		<>
 			<MainNavigation />
 			<main>
+				{navigation.state === 'loading' && <p>Loading...</p>}
 				<Outlet />
 			</main>
 		</>
