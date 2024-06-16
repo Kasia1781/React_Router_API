c29c194cbec99142659167bba6ba6a6bf4631485
Kasia <kasia1781@gmail.com>
2024-06-16 14:28:56 +0200

    added Link in EventsPage component

--- frontend/src/pages/EventsPage.tsx
+++ frontend/src/pages/EventsPage.tsx
@@ -1,3 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const DUMMY_EVENTS = [
+	{ id: 'e1', title: 'Some events' },
+	{ id: 'e2', title: 'Another events' },
+];
+
 export default function EventsPage() {
-	return <h1>Events Page</h1>;
+	return (
+		<>
+			<h1>Events Page</h1>
+			{DUMMY_EVENTS.map((event) => (
+				<li key={event.id}>
+					<Link to={event.id}>{event.title}</Link>
+				</li>
+			))}
+		</>
+	);
 }
