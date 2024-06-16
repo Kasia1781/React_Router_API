df9b34b5368f3e6cbc0f001e9c148cc61adf2d67
Kasia <kasia1781@gmail.com>
2024-06-16 13:55:49 +0200

    added router in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePages';
+import EventsPage from './pages/EventsPage';
+import EventDetailPage from './pages/EventDetailPage';
+import NewEventPage from './pages/NewEventPage';
+import EditEventPage from './pages/EditEventPage';
+
+const router = createBrowserRouter([
+	{ path: '/', element: <HomePage /> },
+	{ path: '/events', element: <EventsPage /> },
+	{ path: '/events/:eventID', element: <EventDetailPage /> },
+	{ path: 'events/new', element: <NewEventPage /> },
+	{ path: 'events/:eventID/edit', element: <EditEventPage /> },
+]);
 
 function App() {
-	return <h1>Hello</h1>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
