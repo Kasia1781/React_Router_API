5d612ce6af68efcac0c75c7ffd25ec4f87e638ed
Kasia <kasia1781@gmail.com>
2024-06-16 19:08:07 +0200

    fetching events in EventsPage component

---
+++ frontend/src/components/EventList.tsx
@@ -0,0 +1,33 @@
+import classes from './EventList.module.css';
+import { Link } from 'react-router-dom';
+
+type EventsProps = {
+	events: {
+		id: string;
+		title: string;
+		data: number;
+		image: string;
+		description: string;
+	};
+};
+
+export default function EventList({ events }: EventsProps) {
+	return (
+		<div className={classes.events}>
+			<h1>All Events</h1>
+			<ul className={classes.list}>
+				{events.map((event) => (
+					<li key={event.id} className={classes.item}>
+						<Link to={event.id}>
+							<img src={event.image} alt={event.title} />
+							<div className={classes.content}>
+								<h2>{event.title}</h2>
+								<time>{event.date}</time>
+							</div>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
--- frontend/src/pages/EventsPage.tsx
+++ frontend/src/pages/EventsPage.tsx
@@ -1,19 +1,50 @@
-import { Link } from 'react-router-dom';
-
-const DUMMY_EVENTS = [
-	{ id: 'e1', title: 'Some events' },
-	{ id: 'e2', title: 'Another events' },
-];
+import { useEffect, useState } from 'react';
+import EventList from '../components/EventList';
 
 export default function EventsPage() {
+	const [eventsFetching, setEventsFetching] = useState<EventsData[] | null>(
+		null
+	);
+	const [error, setError] = useState<string | null>(null);
+	const [isFetching, setIsFetching] = useState(false);
+
+	type EventsData = {
+		id: string;
+		title: string;
+		data: number;
+		image: string;
+		description: string;
+	};
+
+	useEffect(() => {
+		async function fetchEvents() {
+			setIsFetching(true);
+
+			try {
+				const response = await fetch('http://localhost:8080/events');
+
+				if (!response.ok) {
+					throw new Error('Fetching events failed!');
+				}
+
+				const data = (await response.json()) as { events: EventsData[] };
+				setEventsFetching(data.events);
+				setError(null);
+			} catch (error: any) {
+				setError(error.message || 'An unknown error occurred');
+				setEventsFetching(null);
+			}
+			setIsFetching(false);
+		}
+		fetchEvents();
+	}, []);
+
 	return (
 		<>
 			<h1>Events Page</h1>
-			{DUMMY_EVENTS.map((event) => (
-				<li key={event.id}>
-					<Link to={event.id}>{event.title}</Link>
-				</li>
-			))}
+			{isFetching && <p>Loading...</p>}
+			{error && <p>{error}</p>}
+			{eventsFetching && <EventList events={eventsFetching} />}
 		</>
 	);
 }
