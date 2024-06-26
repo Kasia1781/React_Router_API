264547a66701c6216e44a3423211a7f162a9b3a1
Kasia <kasia1781@gmail.com>
2024-06-26 20:14:27 +0200

    added method delete

--- frontend/src/components/EventItem.tsx
+++ frontend/src/components/EventItem.tsx
@@ -14,7 +14,12 @@ type EventProps = {
 
 export default function EventItem({ event }: EventProps) {
 	function startDeleteHandler() {
-		//...
+		const proceed = window.confirm('Are you sure?'); //dostajemy true lub false
+
+		//Wywołujemy akcje i przesyłamy dane za pomocą hook useSubmit(). Następnie submit wywołujemy w if przekazując mu 2 argumenty. Pierwszym są dane które chcemy przesłać, 2 argument to metoda w tym przypadku będzie to delete czyli usunięcie danych.
+		if (proceed) {
+			submit(null, { method: 'delete' }); //nie potrzebujemy 1 argumentu bo nie pobieramy żadnych danych
+		}
 	}
 
 	return (
