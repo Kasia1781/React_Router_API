f8f3de5cdbfff8e8e8afd08dfcdbc78460a8f6e5
Kasia <kasia1781@gmail.com>
2024-06-26 20:16:12 +0200

    added method delete

--- frontend/src/pages/EventDetailPage.tsx
+++ frontend/src/pages/EventDetailPage.tsx
@@ -36,3 +36,16 @@ export async function loader({ params }: { eventId: string }) {
 		return resData.event as EventData;
 	}
 }
+
+export async function action({ params, request }) {
+	const eventId = params.eventId;
+	const response = await fetch('http://localhost:8080/events/' + eventId, {
+		method: request.method,
+	});
+
+	if (!response.ok) {
+		throw json({ message: 'Could not delete event' }, { status: 500 });
+	}
+
+	return redirect('/events');
+}
