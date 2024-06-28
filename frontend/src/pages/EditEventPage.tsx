df454d78a7c8788487fea8ea2eadd9ceafb18cb1
Kasia <kasia1781@gmail.com>
2024-06-28 18:59:44 +0200

    use of method patch

--- frontend/src/pages/EditEventPage.tsx
+++ frontend/src/pages/EditEventPage.tsx
@@ -4,5 +4,5 @@ import EventForm from '../components/EventForm';
 export default function EditEventPage() {
 	const data = useRouteLoaderData('event-detail');
 
-	return <EventForm event={data} />;
+	return <EventForm event={data} method='patch' />; //metoda patch do edycji wydarzenia
 }
